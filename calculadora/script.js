06a785d663c39fab891994ef7a6ef79e9ec8092b
Rodrigo Xavier <rodrigomxsantos@gmail.com>
2023-02-15 20:44:00 -0300

    :hammer: add script js

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,3 +1,26 @@
-function displaynum(n1) {
-  Calculator.text1.value = Calculator.text1.value + n1;
+var text = document.querySelector(".visualizaCalculo");
+var res = document.querySelector(".resultado");
+
+var calculate = (number) => {
+  text.value += number;
+};
+var resultado = () => {
+  try {
+    res.value = eval(text.value);
+  } catch (err) {
+    alert("Operação incorreta");
+  }
+};
+
+function clr() {
+  res.value = "";
+  text.value = "";
+}
+
+function del() {
+  text.value = text.value.slice(0, -1);
+}
+
+function round() {
+  res.value = res.value.tofixed(2);
 }
